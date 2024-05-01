123ede8cec581b052d46386f3b575eeec9e599b4
flavio <flavio.silvapinkman@gmail.com>
2024-05-01 16:44:47 -0300

    created and config OrderStatus enum

--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.config;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null,"Maria","maria@gmail.com","343284732897487324872", "123456");
         User u2 = new User(null,"Carlos","carlos@gmail.com","343287324872", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T14:53:07Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:53:07Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.DELIVERED);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T14:53:07Z"), u2, OrderStatus.DELIVERED);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:53:07Z"), u1, OrderStatus.DELIVERED);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
